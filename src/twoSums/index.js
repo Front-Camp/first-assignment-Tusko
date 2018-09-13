c0dd132c8bf77e20f1788cc528eebab6aded952a
Tusko Trush <tusko@photoinside.me>
2018-09-13 21:44:13 +0300

    twoSums || Console result is ok, tests is failed

--- src/twoSums/index.js
+++ src/twoSums/index.js
@@ -12,17 +12,19 @@
 const twoSums = (arr, base) => {
     var sums = [];
     // check each element in array
-    for (var i = 0; i < arr.length; i++) {
+    for (let i = 0; i < arr.length; i++) {
         // check each other element in the array
-        for (var j = i + 1; j < arr.length; j++) {
-            // determine if these two elements sum to S
+        for (let j = i + 1; j < arr.length; j++) {
+            // determine if these two elements sum to base
             if (arr[i] + arr[j] === base) {
                 sums.push([arr[i], arr[j]]);
             }
         }
     }
     // return all pairs of integers that sum to base
+
     console.log(sums); //console result is ok, test is failed
+
     return sums;
 };
 
